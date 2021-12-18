3c3fa304c3a712a2856b659d4cf87674890d60c4
pringle97 <malia.pringle@gmail.com>
2021-12-18 13:49:33 -0800

    cleaning up files

--- db/index.js
+++
@@ -1,5 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/movies_db')
-
-module.exports = sequelize
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const { prompt } = require("inquirer");
 const cTable = require("console.table");
 const db = ('./db');
 
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/movies_db')
+
+module.exports = sequelize
